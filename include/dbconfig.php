2e1b42af503d3fbcb9b3e6234423b9cdf2b21a76
Adel K. Bargach <adelk.bargach@gmail.com>
2022-03-22 19:49:49 +0100

    .

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -4,11 +4,6 @@ $dbname = 'cinema';
 $username = 'root';
 $password = '';
 
-// $dbhost = 'us-cdbr-east-05.cleardb.net';
-// $dbname = 'heroku_e68502f83c75ed5';
-// $username = 'be68da4fdd55d7';
-// $password = '9883dd4b';
 ?>
 
 
-<!-- mysql://be68da4fdd55d7:9883dd4b@us-cdbr-east-05.cleardb.net/heroku_e68502f83c75ed5?reconnect=true -->
