6f23e39dc2ab400dba743c3cb85e53c49c53b1f7
Adel K. Bargach <adelk.bargach@gmail.com>
2022-03-30 15:56:18 +0200

    deploy-heroku

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -1,15 +1,15 @@
+<!-- <?php
+// $dbhost = 'localhost';
+// $dbname = 'cinema';
+// $username = 'root';
+// $password = '';
+?> -->
+
 <?php
-$dbhost = 'localhost';
-$dbname = 'cinema';
-$username = 'root';
-$password = '';
-
-?>
-
-
-<!-- $dbhost = 'eu-cdbr-west-02.cleardb.net';
+$dbhost = 'eu-cdbr-west-02.cleardb.net';
 $dbname = 'heroku_6730a3d4cf6fbfe';
 $username = 'b42d671c90fa2f';
-$password = 'ab9dbd3c'; -->
+$password = 'ab9dbd3c';
+?>
 
 <!-- mysql://b42d671c90fa2f:ab9dbd3c@eu-cdbr-west-02.cleardb.net/heroku_6730a3d4cf6fbfe?reconnect=true -->
